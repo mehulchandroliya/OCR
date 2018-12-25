5de3ffe9a5363091dff1088f7a596118f73d6a60
Mehul <mehul.chandroliya@gmail.com>
2018-12-25 17:12:20 +0530

    Change List : 1) added Bootstrap 2) Agent Service

---
+++ OCR.Master/OCR.Master.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace OCR.Master.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jQuery/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                       "~/Scripts/bootstrap/bootstrap.bundle.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/fontawesome/css/all.css",
+                "~/Content/bootstrap/bootstrap.css"));
+
+#if DEBUG
+            BundleTable.EnableOptimizations = false;
+#else
+            BundleTable.EnableOptimizations = true;
+#endif
+        }
+    }
+}
